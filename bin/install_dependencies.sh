3fee4a6f289ed14cf0e894b10d8aaecb3be39905
Luc Grosheintz <luc.grosheintz@gmail.com>
2021-02-12 16:43:14 +0100

    Update dependency script.

--- bin/install_dependencies.sh
+++ bin/install_dependencies.sh
@@ -1,8 +1,13 @@
 #! /usr/bin/env bash
 
+set -e
+component_name="ZisaTimeStepping"
+
 if [[ "$#" -lt 2 ]]
 then
     echo "Usage: $0 COMPILER DESTINATION [--zisa_has_mpi=ZISA_HAS_MPI]"
+    echo "                               [--zisa_has_cuda=ZISA_HAS_CUDA]"
+    exit -1
 fi
 
 for arg in "$@"
@@ -10,6 +15,9 @@ do
     case $arg in
         --zisa_has_mpi=*)
             ZISA_HAS_MPI=${arg#*=}
+            ;;
+        --zisa_has_cuda=*)
+            ZISA_HAS_CUDA=${arg#*=}
             ;;
         *)
             ;;
@@ -21,7 +29,11 @@ then
     ZISA_HAS_MPI=0
 fi
 
-component_name="ZisaTimeStepping"
+if [[ -z "${ZISA_HAS_CUDA}" ]]
+then
+    ZISA_HAS_CUDA=1
+fi
+
 if [[ ${ZISA_HAS_MPI} -eq 0 ]]
 then
     zisa_dependencies=("ZisaCore" "ZisaMemory")
@@ -29,28 +41,30 @@ else
     zisa_dependencies=("ZisaCore" "ZisaMemory" "ZisaMPI")
 fi
 
-zisa_memory_root=$(realpath $(dirname $(readlink -f $0))/..)
+zisa_root="$(realpath "$(dirname "$(readlink -f "$0")")"/..)"
 
-install_dir=$(${zisa_memory_root}/bin/install_dir.sh $1 $2 --zisa_has_mpi=${ZISA_HAS_MPI})
-source_dir=${install_dir}/sources
-conan_file=${zisa_memory_root}/conanfile.txt
+CC="$1"
+CXX="$(${zisa_root}/bin/cc2cxx.sh $CC)"
 
-if [[ -f $conan_file ]]
-then
-   mkdir -p ${install_dir}/conan && cd ${install_dir}/conan
-   conan install $conan_file -s compiler.libcxx=libstdc++11
-fi
+install_dir="$("${zisa_root}/bin/install_dir.sh" "$1" "$2" --zisa_has_mpi=${ZISA_HAS_MPI})"
+source_dir="${install_dir}/sources"
+conan_file="${zisa_root}/conanfile.txt"
 
-mkdir -p ${source_dir}
+mkdir -p "${install_dir}/conan" && cd "${install_dir}/conan"
+conan install "$conan_file" \
+        -s compiler=$(basename "${CC}") \
+        -s compiler.libcxx=libstdc++11
+
+mkdir -p "${source_dir}"
 for dep in "${zisa_dependencies[@]}"
 do
-    src_dir=${source_dir}/$dep
+    src_dir="${source_dir}/$dep"
     repo_url=git@github.com:1uc/${dep}.git
 
     # If necessary and reasonable remove ${src_dir}.
-    if [[ -d ${src_dir} ]]
+    if [[ -d "${src_dir}" ]]
     then
-        cd ${src_dir}
+        cd "${src_dir}"
 
         if [[ -z $(git remote -v 2>/dev/null | grep ${repo_url}) ]]
         then
@@ -58,22 +72,24 @@ do
             exit -1
 
         else
-            cd ${HOME}
-            rm -rf ${src_dir}
+            cd "${HOME}"
+            rm -rf "${src_dir}"
         fi
     fi
 
-    git clone ${repo_url} ${src_dir}
+    git clone ${repo_url} "${src_dir}"
 
-    mkdir -p ${src_dir}/build-dep
-    cd ${src_dir}/build-dep
+    mkdir -p "${src_dir}/build-dep"
+    cd "${src_dir}/build-dep"
 
-    cmake -DCMAKE_INSTALL_PREFIX=${install_dir}/zisa \
-          -DCMAKE_PREFIX_PATH=${install_dir}/zisa/lib/cmake/zisa \
-          -DCMAKE_MODULE_PATH=${install_dir}/conan \
-          -DCMAKE_PROJECT_${dep}_INCLUDE=${install_dir}/conan/conan_paths.cmake \
-          -DZISA_HAS_MPI=${ZISA_HAS_MPI} \
-          -DCMAKE_BUILD_TYPE=Release \
+    cmake -DCMAKE_INSTALL_PREFIX="${install_dir}/zisa" \
+          -DCMAKE_PREFIX_PATH="${install_dir}/zisa/lib/cmake/zisa" \
+          -DCMAKE_MODULE_PATH="${install_dir}/conan" \
+          -DCMAKE_PROJECT_${dep}_INCLUDE="${install_dir}/conan/conan_paths.cmake" \
+          -DCMAKE_C_COMPILER="${CC}" \
+          -DCMAKE_CXX_COMPILER="${CXX}" \
+          -DZISA_HAS_MPI="${ZISA_HAS_MPI}" \
+          -DCMAKE_BUILD_TYPE="Release" \
           ..
 
     cmake --build . --parallel $(nproc)
@@ -81,4 +97,13 @@ do
 done
 
 echo "The dependencies were installed at"
-echo "    ${install_dir}"
+echo "    export DEP_DIR=${install_dir}"
+echo ""
+echo "Use"
+echo "    cmake -DCMAKE_PROJECT_${component_name}_INCLUDE=${install_dir}/conan/conan_paths.cmake \\ "
+echo "          -DCMAKE_MODULE_PATH=${install_dir}/conan \\ "
+echo "          -DCMAKE_PREFIX_PATH=${install_dir}/zisa/lib/cmake/zisa \\ "
+echo "          -DCMAKE_C_COMPILER=${CC} \\ "
+echo "          -DCMAKE_CXX_COMPILER=${CXX} \\ "
+echo "          -DZISA_HAS_MPI=${ZISA_HAS_MPI} \\ "
+echo "          REMAINING_ARGS "
