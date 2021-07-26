526d4f874ac2b0cff828366d7d62e5cd9363fc4c
Luc Grosheintz <luc.grosheintz@gmail.com>
2021-07-26 14:15:30 +0200

    Improve .

--- bin/install_dependencies.sh
+++ bin/install_dependencies.sh
@@ -131,6 +131,7 @@ do
 
     ${CMAKE} -DCMAKE_INSTALL_PREFIX="${install_dir}/zisa" \
           -DCMAKE_PREFIX_PATH="${install_dir}/zisa/lib/cmake/zisa" \
+          -DCMAKE_MODULE_PATH="${install_dir}/zisa/lib/cmake/zisa" \
           -DCMAKE_PROJECT_${dep}_INCLUDE="${install_dir}/conan/conan_paths.cmake" \
           -DCMAKE_C_COMPILER="${CC}" \
           -DCMAKE_CXX_COMPILER="${CXX}" \
@@ -149,6 +150,7 @@ echo "Use"
 echo "    ${CMAKE} \ "
 echo "        -DCMAKE_PROJECT_${component_name}_INCLUDE=${install_dir}/conan/conan_paths.cmake \ "
 echo "        -DCMAKE_PREFIX_PATH=${install_dir}/zisa/lib/cmake/zisa \ "
+echo "        -DCMAKE_MODULE_PATH=${install_dir}/zisa/lib/cmake/zisa \ "
 echo "        -DCMAKE_C_COMPILER=${CC} \ "
 echo "        -DCMAKE_CXX_COMPILER=${CXX} \ "
 echo "        -DZISA_HAS_CUDA=${ZISA_HAS_CUDA} \ "
